625e5e8e7b5772767bfdd441ba73ba5ec8c21ca4
Teddy <pazteddy@gmail.com>
2024-09-09 18:54:57 -0400

    ADD initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hola desde Typescript");
+function combine(a, b) {
+    return a + b;
+}
+var result = combine(10, 20);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,7 @@
+console.log("Hola desde Typescript");
+function combine(a: number, b: number) {
+  return a + b;
+}
+
+const result = combine(10, 20);
+console.log(result);
