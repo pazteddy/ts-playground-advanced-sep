d27dc3930bffc081191af4466eb19a0f5f237020
Teddy <pazteddy@gmail.com>
2024-09-09 20:34:16 -0400

    UPDATE return function combine

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ console.log("Hola desde Typescript");
 function combine(a, b) {
     return a + b;
 }
-var result = combine(10, 20);
+var result = combine(2, 3);
 console.log(result);
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 console.log("Hola desde Typescript");
-function combine(a: number, b: number) {
+function combine(a: number, b: number): number {
   return a + b;
 }
 
-const result = combine(10, 20);
+const result = combine(2, 3);
 console.log(result);
