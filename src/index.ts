ed521cfae68edbf6ec156c7fa32ad10ef141ad04
Teddy <pazteddy@gmail.com>
2024-09-09 21:49:56 -0400

    UPDATE adding examples

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,13 @@ import combine from "./combine";
 console.log("Hola desde Typescript");
 const result = combine(2, 3);
 console.log(result);
+const temperature = 45;
+console.log(temperature);
+
+let age: any;
+
+age = 23;
+age = "vienti uno";
+age.hacerAlgo();
+
+console.log(age);
